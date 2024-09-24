a2dab161ed2123463abe252cd52147dd5319ebff
xjf <peak1203@126.com>
2024-09-24 14:17:39 +0800

    feat: update time_tool.py

--- flaskr/tool/time_tool.py
+++ flaskr/tool/time_tool.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def datetime_to_str(timestamp):
@@ -7,12 +7,21 @@ def datetime_to_str(timestamp):
     formatted_completion_time = dt_object.strftime('%Y-%m-%d %H:%M:%S')
     return formatted_completion_time
 
+def date_to_string(date):
+    return date.strftime('%Y-%m-%d %H:%M:%S')
+
 
 def str_to_datetime(date_str):
     # 将'yyyy-MM-dd HH'格式转换为时间戳
     time = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
     return time
-    
+
+def get_current_date():
+    return datetime.now()
+
 def get_datetime_str():
     time = datetime.now()
     return time.strftime('%Y-%m-%d %H:%M:%S')
+
+def get_date_after_days(days):
+    return datetime.now() + timedelta(days=days)

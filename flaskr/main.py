699fbc00397d61d784f5824037bd770e52a15f17
JF.Hsiong <peak1203@126.com>
2025-04-01 15:45:33 +0800

    feat: wsgi to asgi

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,6 +1,7 @@
+from asgiref.wsgi import WsgiToAsgi
 from uvicorn import Config, Server
 
-from flaskr.route.task_pdf_routes import *
+from flaskr.route.recognition_routes import *
 from flaskr.tool.flask_tool import create_app
 
 if __name__ == '__main__':
@@ -9,7 +10,12 @@ if __name__ == '__main__':
     # 注册蓝图
     app.register_blueprint(api, url_prefix="/" + default_config.CONTEXT_PATH)
     
+    # 转换 asgi
+    asgi_app = WsgiToAsgi(app)
+    
     # 启动服务 - 支持多线程
     print("Start server at 127.0.0.1:" + str(args.port) + "/" + default_config.CONTEXT_PATH)
-    config = Config(app=app, host="0.0.0.0", port=args.port)
+    print()
+    config = Config(app=asgi_app, host="0.0.0.0", port=args.port)
     server = Server(config)
+    server.run()
